425213b3d4dba8cd470a6728d2e7e6be47bd186b
kimba <skyqwn@gmail.com>
2022-09-30 11:39:44 +0900

    env 추가

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`서버가 ${port}에서 실행중입니다.`);
 });
